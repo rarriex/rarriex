b546b6870fae3a3ea076bb945abb2fbfa30a014d
rarriex <49093574+rarriex@users.noreply.github.com>
2019-03-30 14:38:50 +0400

    Add files via upload

---
+++ account.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html class="js" lang="en-US" style="opacity: 1; visibility: visible;"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+<title>farmbets.net | 502: Bad gateway</title>
+
+
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+<link rel="stylesheet" id="cf_styles-css" href="/Content/502/cf.errors.css" type="text/css" media="screen,projection">
+<style type="text/css">body{margin:0;padding:0}</style>
+<!--<script type="text/javascript" src="/Content/502/zepto.min.js"></script>
+<script type="text/javascript" src="/Content/502/cf.common.js"></script>-->
+
+</head>
+<body><div id="cf_alert_div" style="margin: 0;padding: 0;position: relative;top: 0;left: 0;display: block; z-index: 1000"><div style="margin: 0px;padding: 10px;border-bottom: 1px solid #a5a5a5;background: #f0f0f0;font-weight: normal;text-align: left;"><table width="100%"><tbody><tr><td valign="top"><a href="#" onclick="document.cookie=&#39;cf_use_ob=0;path=/&#39;;window.location.reload();return false;" style="margin: 0 20px 0 0;padding: 10px 0 0 20px;text-decoration: none;font: 12px Helvetica,Arial,sans-serif;color: #eff7e8;font-weight: bold;text-align: left;position: relative;display: inline-block;height: 38px;background: url(/Content/502/retry.png) no-repeat 0 0;overflow: visible;font-size: 14px;line-height: 1.2em;text-shadow: #437b0a 0 1px 0;white-space: nowrap;">Retry for a live version<span style="margin: 0;padding: 0;position: absolute;top: 0;left: 100%;width: 20px;height: 38px;background: url(/Content/502/retry.png) no-repeat 100% -38px;"></span></a> <noscript style="margin: 10px 0 10px 0;padding: 0;font: 12px Helvetica,Arial,sans-serif;color: #404040;display: block;">(Enable Javascript first.)</noscript></td><td valign="middle" style="margin: 0;padding: 0;"><p style="margin: 0 0 0 20px;padding: 0;font: 12px Helvetica,Arial,sans-serif;color: #404040;">This page (<a href="#" style="margin: 0;padding: 0;text-decoration: underline;font: 12px Helvetica,Arial,sans-serif;color: #007ed9;font-weight: normal;text-align: left;">https://farmbets.net</a>) is currently offline. However, because the site uses Cloudflare's Always Online™ technology you can continue to surf a snapshot of the site. We will keep checking in the background and, as soon as the site comes back, you will automatically be served the live version. <span id="caption" style="margin: 0;padding: 0;color: #a5a5a5;">Always Online™ is powered by <a href="https://www.cloudflare.com/5xx-error-landing?utm_source=ao_banner" target="_blank" style="margin: 0;padding: 0;text-decoration: underline;font: 12px Helvetica,Arial,sans-serif;color: #007ed9;font-weight: normal;text-align: left;">Cloudflare</a> | <a href="javascript:void(null);" onclick="document.getElementById(&#39;cf_alert_div&#39;).style.display=&#39;none&#39;;" style="margin: 0;padding: 0;text-decoration: underline;font: 12px Helvetica,Arial,sans-serif;color: #007ed9;font-weight: normal;text-align: left;">Hide this Alert</a></span></p></td></tr></tbody></table></div></div>
+<div id="cf-wrapper">
+
+    
+
+    <div id="cf-error-details" class="cf-error-details-wrapper">
+        <div class="cf-wrapper cf-error-overview">
+            <h1>
+              <span class="cf-error-type" data-translate="error">Error</span>
+              <span class="cf-error-code">502</span>
+              <small class="heading-ray-id">Ray ID: 2a3v1cdmhy9yflor • 2018-01-06 19:01:05 UTC</small>
+            </h1>
+            <h2 class="cf-subheadline" data-translate="error_desc">Bad gateway</h2>
+        </div><!-- /.error-overview -->
+
+        <div class="cf-section cf-highlight cf-status-display">
+            <div class="cf-wrapper">
+                <div class="cf-columns cols-3">
+                  
+<div id="cf-browser-status" class="cf-column cf-status-item cf-browser-status ">
+  <div class="cf-icon-error-container">
+    <i class="cf-icon cf-icon-browser"></i>
+    <i class="cf-icon-status cf-icon-ok"></i>
+  </div>
+  <span class="cf-status-desc" data-translate="browser_desc">You</span>
+  <h3 class="cf-status-name" data-translate="browser_label">Browser</h3>
+  <span class="cf-status-label" data-translate="browser_status_label">Working</span>
+</div>
+
+<div id="cf-cloudflare-status" class="cf-column cf-status-item cf-cloudflare-status ">
+  <div class="cf-icon-error-container">
+    <i class="cf-icon cf-icon-cloud"></i>
+    <i class="cf-icon-status cf-icon-ok"></i>
+  </div>
+  <span class="cf-status-desc" data-translate="cloud_desc">Frankfurt</span>
+  <h3 class="cf-status-name" data-translate="cloud_label">Cloudflare</h3>
+  <span class="cf-status-label" data-translate="cloud_status_label">Working</span>
+</div>
+
+<div id="cf-host-status" class="cf-column cf-status-item cf-host-status cf-error-source">
+  <div class="cf-icon-error-container">
+    <i class="cf-icon cf-icon-server"></i>
+    <i class="cf-icon-status cf-icon-error"></i>
+  </div>
+  <span class="cf-status-desc" data-translate="server_desc">farmbets.net</span>
+  <h3 class="cf-status-name" data-translate="server_label">Host</h3>
+  <span class="cf-status-label" data-translate="server_status_label">Error</span>
+</div>
+
+                </div>
+            </div>
+        </div><!-- /.status-display -->
+
+        <div class="cf-section cf-wrapper">
+            <div class="cf-columns two">
+                <div class="cf-column">
+                    <h2 data-translate="what_happened">What happened?</h2>
+                    <p>The web server reported a bad gateway error.</p>
+                </div>
+
+                <div class="cf-column">
+                    <h2 data-translate="what_can_i_do">What can I do?</h2>
+                    <p data-translate="try_again_in_a_few">Please try again in a few minutes.</p>
+                </div>
+            </div>
+        </div><!-- /.section -->
+
+        <div class="cf-error-footer cf-wrapper">
+  <p>
+    <span class="cf-footer-item">Cloudflare Ray ID: <strong>2a3v1cdmhy9yflor</strong></span>
+    <span class="cf-footer-separator">•</span>
+    <span class="cf-footer-item"><span data-translate="your_ip">Your IP</span>: 46.56.66.64, 46.56.66.64</span>
+    <span class="cf-footer-separator">•</span>
+    <span class="cf-footer-item"><span data-translate="performance_security_by">Performance &amp; security by</span> <a data-orig-proto="https" data-orig-ref="www.cloudflare.com/5xx-error-landing?utm_source=error_footer" id="brand_link" target="_blank" href="https://www.cloudflare.com/5xx-error-landing?utm_source=error_footer">Cloudflare</a></span>
+    
+  </p>
+</div><!-- /.error-footer -->
+
+
+    </div><!-- /#cf-error-details -->
+</div><!-- /#cf-wrapper -->
+</body></html>
